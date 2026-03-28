631ff5135ecb1b06b07a3a56fe10dcb0ddbcfe52
Infinite0327 <1969629453@qq.com>
2026-03-28 14:41:49 +0800

    实现了对前端json形式的请求的接收

--- main.go
+++ main.go
@@ -3,11 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
 )
 
+type CalcRequest struct {
+	Expression string `json:"expression"` 
+}
+
+type CalcResponse struct {
+	Answer int `json:"answer"`
+}
+
 func pingHandler(w http.ResponseWriter,r *http.Request){
-	fmt.Println("收到了一次访问")
-	w.Write([]byte("Hello,this is ping route"))
+	var req CalcRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "请求格式不对", http.StatusBadRequest)
+		return
+	}
+	fmt.Println("成功获取前端请求：", req.Expression)
+	fakeResult := 10000
+	resp := CalcResponse{
+		Answer: fakeResult,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
 }
 
 func main(){
